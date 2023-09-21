78fd502000589e7dd3ae1d624a40e4eb9166d529
Deepak Singh <63626210+hey-its-d2t2@users.noreply.github.com>
2023-09-21 22:43:53 +0530

    Add files via upload

---
+++ C-Socket_Programs/TCP_Client.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+int main()
+{
+	//create a socket
+	int network_socket;
+	network_socket = socket(AF_INET, SOCK_STREAM,0);
+	
+	//Specify an address for the socket
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	
+	int connectio_status = (network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	
+	if(connectio_status == -1)
+	{
+		printf("There was an error making a connectio to the remote socket !! \n\n");
+	}
+	
+	//recive data from the server
+	char server_response[256];
+	recv(network_socket, &server_response, sizeof(server_response),0);
+	
+	//print the server response
+	printf("The server sent the data : %s\n\n",server_response);
+	
+	//close the socket
+	//close(network_socket);
+	
+	return 0;
+}
